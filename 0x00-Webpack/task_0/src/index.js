802a752f3714d66cfe507155e8e5d5a4b4cb024e
OjElsa <elsahojwang06@gmail.com>
2024-07-05 12:49:05 +0300

    task 0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+import $ from 'jquery'; // Import jQuery
+
+$('body').append([
+    $('<p>').text('Holberton Dashboard'),
+    $('<p>').text('Dashboard data for the students'),
+    $('<p>').text('Copyright - Holberton School')
+]);
+
